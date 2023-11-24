7f0d2743fe4f33be5a8d22f5bb004888691b8e09
Konstantin Parhoc <konstantinparhoc@gmail.com>
2023-11-24 13:36:49 +0300

    add Comment serializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Review
+from reviews.models import Comment, Review, User
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -40,7 +40,8 @@ class ReviewSerializer(serializers.ModelSerializer):
         """
         Validate that `author` and `title` pair is not in DB.
         """
-        attrs['author'] = self.context['request'].user
+        # attrs['author'] = self.context['request'].user
+        attrs['author'] = User.objects.get(pk=1)
         attrs['title'] = self.context['request'].parser_context['kwargs'].get(
             'title_id')
         if Review.objects.filter(
@@ -51,3 +52,32 @@ class ReviewSerializer(serializers.ModelSerializer):
                 'User can have only one review for a single title.'
             )
         return attrs
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Comment model serializer.
+
+    Include fields:
+    * id (read only);
+    * text;
+    * author (read only) - string username;
+    * pub_date (read only).
+    """
+
+    author = serializers.SlugField(
+        'username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = (
+            'id',
+            'text',
+            'author',
+            'pub_date',
+        )
+        read_only_fields = (
+            'pub_date',
+        )
