11f9e24f2b3316c3885673482df4a22bd9b63813
Konstantin Parhoc <konstantinparhoc@gmail.com>
2023-11-24 13:36:59 +0300

    add Comment url

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import ReviewViewSet
+from .views import CommentViewSet, ReviewViewSet
 
 app_name = 'api-v1'
 
@@ -10,6 +10,11 @@ router_api_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
     'review'
+)
+router_api_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    'comment'
 )
 
 urlpatterns = [
